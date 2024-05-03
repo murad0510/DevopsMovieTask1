4d23ce532d16448b731a8c13fea8a98ac4473b9b
Azizo_pu01 <Azizo_pu01@itstep.edu.az>
2024-05-03 11:39:05 +0400

    Solved

--- ShowMovie/Controllers/HomeController.cs
+++ ShowMovie/Controllers/HomeController.cs
@@ -8,10 +8,10 @@ namespace ShowMovie.Controllers
 {
     public class HomeController : Controller
     {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis-12939.c251.east-us-mz.azure.redns.redis-cloud.com:12939,password=gQEQnmuLQyxtwFApDk2TmWiYPyd6830N");
 
         public async Task<IActionResult> Index()
         {
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis-12939.c251.east-us-mz.azure.redns.redis-cloud.com:12939,password=gQEQnmuLQyxtwFApDk2TmWiYPyd6830N");
             var db = redis.GetDatabase();
 
             var hashEntries = await db.HashGetAllAsync("movies");
@@ -37,6 +37,36 @@ namespace ShowMovie.Controllers
             };
 
             return View(model);
+        }
+
+        public async Task<IActionResult> DeleteMovie()
+        {
+            var db = redis.GetDatabase();
+            var hashEntries = await db.HashGetAllAsync("movies");
+
+            List<Movie> movieList = new List<Movie>();
+
+
+            foreach (var hashEntry in hashEntries)
+            {
+                string title = hashEntry.Name;
+                string poster = hashEntry.Value;
+
+                var movie = new Movie
+                {
+                    Name = title,
+                    Poster = poster,
+                };
+
+                movieList.Add(movie);
+            }
+
+            if (movieList.Count > 0)
+            {
+                db.HashDelete("movies", movieList[0].Name);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- ShowMovie/wwwroot/js/site.js
+++ ShowMovie/wwwroot/js/site.js
@@ -1,6 +1,8 @@
 ﻿
 
-setInterval(InvokeHomeOndex, 20000);
+setInterval(InvokeHomeOndex, 10000);
+setInterval(DeleteMovie, 30000);
+
 function InvokeHomeOndex() {
     $.ajax({
         url: `/Home/Index`,
@@ -12,3 +14,13 @@ function InvokeHomeOndex() {
     })
 }
 
+function DeleteMovie() {
+    $.ajax({
+        url: `/Home/DeleteMovie`,
+        method: "GET",
+
+        success: function (data) {
+            //console.log("s");
+        }
+    })
+}
